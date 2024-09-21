4d461f29adcfd2a3ae83c937cbb36e4f8099f0dd
adymar73583418 <adymarr2005@gmail.com>
2024-09-21 18:11:15 +0600

    Search bar added

--- src/app/temp_data/main_page_data.jsx
+++ src/app/temp_data/main_page_data.jsx
@@ -1,7 +1,45 @@
 data = [
     {
-        
-    }
+        name: "Свитер с молнией белый",
+        price: 690,
+        rating: 4.7
+    },
+    {
+        name: "Нефорская кофта Х/Б",
+        price: 280,
+        rating: 4.0
+    },
+    {
+        name: "Педали Loro Piana",
+        price: 7900,
+        rating: 5.0,
+    },
+    {
+        name: "Микро Вафли",
+        price: 920,
+        rating: 4.4
+    },
+    {
+        name: "Педали Loro Piana",
+        price: 7900,
+        rating: 5.0,
+    },
+    {
+        name: "Свитер с молнией белый",
+        price: 690,
+        rating: 4.7
+    },
+    {
+        name: "Микро Вафли",
+        price: 920,
+        rating: 4.4
+    },
+    {
+        name: "Нефорская кофта Х/Б",
+        price: 280,
+        rating: 4.0
+    },
+
 ]
 
 export default data
