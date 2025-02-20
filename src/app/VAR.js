8adeec106f8b367689974325b7ceff1a23f747a2
adymar73583418 <adymarr2005@gmail.com>
2025-02-20 20:22:34 +0600

    0.3.0.1

--- src/app/VAR.js
+++ src/app/VAR.js
@@ -1,4 +1,4 @@
-export const BACK_URL = "http://127.0.0.1:8000"
-// export const BACK_URL = "https://optal.ru"
+// export const BACK_URL = "http://127.0.0.1:8000"
+export const BACK_URL = "https://optal.ru"
 
 
