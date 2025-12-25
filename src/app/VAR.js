56ff3ddf20fc3efaf65afff645a9144be9b6456a
Abror <mmtklvabrr@gmail.com>
2025-12-25 17:36:44 +0600

    Applied to sslip

--- src/app/VAR.js
+++ src/app/VAR.js
@@ -1,5 +1,6 @@
 // export const BACK_URL = "http://127.0.0.1:8000"
 // export const BACK_URL = "https://optal.ru"
-export const BACK_URL = "http://176.126.164.86:1337"
+export const BACK_URL = "https://176-126-164-86.sslip.io/"
+
 
 
